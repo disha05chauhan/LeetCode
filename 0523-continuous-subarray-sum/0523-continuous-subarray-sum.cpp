855a534fefb18254f8bcd947ce774a5530804db1
Disha Chauhan <92150243+disha05chauhan@users.noreply.github.com>
2023-06-29 16:56:01 +0530

    Time: 279 ms (23.75%), Space: 113.8 MB (67.54%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+     int n=nums.size();
+        unordered_set<int> sets;
+        long long curr=0,last=0;
+        for(int i=0;i<n;i++){
+            curr+=nums[i];
+            int need=curr%k;
+            if(sets.count(need))return true;
+            sets.insert(last%k);
+            last=curr;
+        }
+        return false;
+    }
+};
