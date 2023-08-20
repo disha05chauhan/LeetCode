1b657a958020ea96838b4d9325ccf92d3dc05d25
Disha Chauhan <92150243+disha05chauhan@users.noreply.github.com>
2023-08-21 01:21:32 +0530

    Time: 1609 ms (92.58%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,16 @@
-SELECT 
-    contest_id,
-    ROUND((COUNT(DISTINCT user_id) * 100.0 / (SELECT COUNT(*) FROM Users)), 2) AS percentage
-FROM 
-    Register
-GROUP BY 
-    contest_id
-ORDER BY 
-    percentage DESC, 
-    contest_id ASC;
+WITH ContestCounts AS (
+    SELECT contest_id, COUNT(DISTINCT user_id) AS registered_users
+    FROM Register
+    GROUP BY contest_id
+)
+
+, TotalUsers AS (
+    SELECT COUNT(*) AS total_users
+    FROM Users
+)
+
+SELECT c.contest_id,
+       ROUND((CAST(c.registered_users AS FLOAT) / t.total_users) * 100, 2) AS percentage
+FROM ContestCounts c
+CROSS JOIN TotalUsers t
+ORDER BY percentage DESC, c.contest_id ASC;
